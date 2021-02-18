34fc4577af2a0f5747b8ab1a6375b138712e9a5e
Olu <theoafactor@gmail.com>
2021-02-18 14:41:40 +0100

    Second commit

--- js/intro.js
+++ js/intro.js
@@ -1,4 +1,5 @@
 const switchBtn = document.getElementsByClassName("switch-btn")[0];
+const container = document.getElementsByClassName("container")[0];
 
 switchBtn.addEventListener("click", function(){
 
@@ -9,4 +10,25 @@ switchBtn.addEventListener("click", function(){
 
 	this.classList.toggle("switched-off-btn");
 
+	this.setAttribute("onclick", "switchOff()");
+
+	container.style.background = "transparent";
+
+
 })
+
+
+function switchOff(){
+	//at this point, the switched-off-btn should have been added
+	switchBtn.classList.remove('switched-off-btn');
+	switchBtn.style.right = "50%";
+	switchBtn.style.left = "0";
+	switchBtn.innerHTML = "Switch On &rightarrow;";
+	switchBtn.removeAttribute("onclick");
+
+	container.style.background = "orange";
+
+
+}
+
+
